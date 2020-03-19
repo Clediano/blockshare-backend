621a12b8cd050e88d465f219ea13531c5307f192
clediano estefenon <cledianoestefenon@gmail.com>
2020-03-18 23:08:42 -0300

    AJustes no arquivo de configuração

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,13 @@
-const { db_name, db_user, db_dialect, db_host, db_pass } = require('./secret');
 
 module.exports = {
     development: {
-        username: db_user,
-        password: db_pass,
-        database: db_name,
-        host: db_host,
-        dialect: db_dialect,
+        username: 'postgres',
+        password: 'postgres',
+        database: 'database',
+        host: 'localhost',
+        dialect: 'postgres',
     },
     production: {
-        username: db_user,
-        password: db_pass,
-        database: db_name,
-        host: db_host,
-        dialect: db_dialect,
+        use_env_variable: "DATABASE_URL"
     }
 };
